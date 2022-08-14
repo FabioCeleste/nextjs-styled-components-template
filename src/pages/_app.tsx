c9977afa20e4c17f47dcb5ffc22d3daf0d234c2c
Fabio da Silva Celeste <fabio.celeste@softwillians.com.br>
2022-08-14 18:29:53 -0300

    add styled

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import GlobalStyle from "./globalStyles";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default MyApp;
