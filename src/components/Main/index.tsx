246ec1ecd9d04a1e5df794add54180c210abf847
Fabio da Silva Celeste <fabio.celeste@softwillians.com.br>
2022-08-17 23:57:23 -0300

    add main

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <div>
+        <h1>
+          Front End <br /> Developer
+        </h1>
+        <img src="/images/main.svg" />
+      </div>
+    </Container>
+  );
+};
+
+export default Main;
